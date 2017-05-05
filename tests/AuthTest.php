e67d5dbb6793de490d2d758bf95835da3987ee80
Mike Mollick <mollick2@gmail.com>
2017-05-05 12:43:17 -0400

    Modifying auth class to work with V4 auth schema

--- tests/AuthTest.php
+++ tests/AuthTest.php
@@ -2,19 +2,14 @@
 
 class AuthTest extends \PHPUnit_Framework_TestCase {
 
-    public function testGetUrl()
+    public function testApiKeySetterGetter()
     {
-        $this->assertEquals('https://api.neverbounce.com/v3/', Auth::getUrl());
+        $this->assertNull(Auth::getApiKey());
 
-        Auth::setVersion('v4');
-        $this->assertEquals('https://api.neverbounce.com/v4/', Auth::getUrl());
+        Auth::setApiKey('abc');
+        $this->assertEquals('abc', Auth::getApiKey());
 
-        Auth::setRouter('api2');
-        $this->assertEquals('https://api2.neverbounce.com/v4/', Auth::getUrl());
-
-        Auth::setRouter(Auth::DEFAULT_ROUTER);
-        Auth::setVersion(Auth::DEFAULT_VERSION);
-        $this->assertEquals('https://api.neverbounce.com/v3/', Auth::getUrl());
+        Auth::setApiKey('123');
+        $this->assertEquals('123', Auth::getApiKey());
     }
-
 }
