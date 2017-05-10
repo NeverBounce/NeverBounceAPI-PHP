f6fba514b29f8010c2945fcbedfda6f78e446aa5
Mike Mollick <mollick2@gmail.com>
2017-05-10 11:03:47 -0400

    Adding support for Single endpoint with examples

---
+++ examples/single-check-address-info.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Authentication is setup in bootstrap file
+ */
+require_once __DIR__ . '/bootstrap.php';
+
+// Verify a single email
+$verifiation = \NeverBounce\Single::verifyWithAddressInformation('mike@neverbounce.com');
+
+// Get verified email
+fwrite(STDOUT, 'Email Verified: ' . $verifiation->email . PHP_EOL);
+
+// Get numeric verification result
+fwrite(STDOUT, 'Numeric Code: ' . $verifiation->result_integer . PHP_EOL);
+
+// Get text based verification result
+fwrite(STDOUT, 'Text Code: ' . $verifiation->result . PHP_EOL);
+
+// Check for dns flag
+fwrite(STDOUT, 'Has DNS: ' . (string) $verifiation->hasFlag('has_dns') . PHP_EOL);
+
+// Check for free_email_host flag
+fwrite(STDOUT, 'Is free mail: ' . (string) $verifiation->hasFlag('free_email_host') . PHP_EOL);
+
+// Get numeric verification result
+fwrite(STDOUT, 'Suggested Correction: ' . $verifiation->suggested_correction . PHP_EOL);
+
+// Check if email is valid
+fwrite(STDOUT, 'Is valid: ' . (string) $verifiation->is('unknown') . PHP_EOL);
+
+// Get numeric verification result
+fwrite(STDOUT, 'Isn\'t valid or catchall: ' . (string) $verifiation->not(['valid', 'catchall']) . PHP_EOL);
+
+// Get credits used
+fwrite(STDOUT, 'Credits used: ' . ($verifiation->credits_info->paid_credits_used + $verifiation->credits_info->free_credits_used) . PHP_EOL);
+
+// Get host
+fwrite(STDOUT, 'Email host: ' . $verifiation->address_info->host . PHP_EOL);
--- src/Single.php
+++ src/Single.php
@@ -1,18 +1,45 @@
 <?php namespace NeverBounce;
 
-use NeverBounce\Object\Verification;
+use NeverBounce\Object\SingleVerification;
 
-class Single extends ApiClient {
-
+class Single extends ApiClient
+{
     /**
      * @param $email
-     * @return Verification
+     * @return SingleVerification
+     * @throws \NeverBounce\Errors\ThrottleException
+     * @throws \NeverBounce\Errors\HttpClientException
+     * @throws \NeverBounce\Errors\GeneralException
+     * @throws \NeverBounce\Errors\BadReferrerException
+     * @throws \NeverBounce\Errors\AuthException
      */
     public static function verify($email)
     {
-        $api = new self();
-        $res = $api->request('single', ['email' => $email]);
-        return new Verification($email, $res['result']);
+        self::$lastInstance = $obj = new self();
+        $res = $obj->request('GET', 'single/check', [
+            'email' => $email,
+            'credits_info' => true,
+        ]);
+        return new SingleVerification($email, $res);
     }
 
+    /**
+     * @param $email
+     * @return SingleVerification
+     * @throws \NeverBounce\Errors\ThrottleException
+     * @throws \NeverBounce\Errors\HttpClientException
+     * @throws \NeverBounce\Errors\GeneralException
+     * @throws \NeverBounce\Errors\BadReferrerException
+     * @throws \NeverBounce\Errors\AuthException
+     */
+    public static function verifyWithAddressInformation($email)
+    {
+        self::$lastInstance = $obj = new self();
+        $res = $obj->request('GET', 'single/check', [
+            'email' => $email,
+            'credits_info' => true,
+            'address_info' => true,
+        ]);
+        return new SingleVerification($email, $res);
+    }
 }
