c1f4da1cc8f17b323890b4cc5e1de259ca913c5e
Mike Mollick <mollick2@gmail.com>
2017-08-15 10:44:49 -0400

    Simplfying single/check endpoint to a single method

--- src/Single.php
+++ src/Single.php
@@ -6,7 +6,9 @@ class Single extends ApiClient
 {
     /**
      * @param $email
-     * @param int $maxexecution
+     * @param int|null $addressinfo
+     * @param int|null $creditsinfo
+     * @param int|null $maxexecution
      * @return VerificationObject
      * @throws \NeverBounce\Errors\ThrottleException
      * @throws \NeverBounce\Errors\HttpClientException
@@ -14,35 +16,15 @@ class Single extends ApiClient
      * @throws \NeverBounce\Errors\BadReferrerException
      * @throws \NeverBounce\Errors\AuthException
      */
-    public static function verify($email, $maxexecution = 60)
+    public static function check($email, $addressinfo = null, $creditsinfo = null, $maxexecution = null)
     {
         self::$lastInstance = $obj = new self();
         $res = $obj->request('GET', 'single/check', [
             'email' => $email,
+            'address_info' => $addressinfo,
+            'credits_info' => $creditsinfo,
             'max_execution_time' => $maxexecution,
             'credits_info' => true,
-        ]);
-        return new VerificationObject($email, $res);
-    }
-
-    /**
-     * @param string $email
-     * @param int $maxexecution
-     * @return VerificationObject
-     * @throws \NeverBounce\Errors\ThrottleException
-     * @throws \NeverBounce\Errors\HttpClientException
-     * @throws \NeverBounce\Errors\GeneralException
-     * @throws \NeverBounce\Errors\BadReferrerException
-     * @throws \NeverBounce\Errors\AuthException
-     */
-    public static function verifyWithAddressInformation($email, $maxexecution = 60)
-    {
-        self::$lastInstance = $obj = new self();
-        $res = $obj->request('GET', 'single/check', [
-            'email' => $email,
-            'max_execution_time' => $maxexecution,
-            'credits_info' => true,
-            'address_info' => true,
         ]);
         return new VerificationObject($email, $res);
     }
